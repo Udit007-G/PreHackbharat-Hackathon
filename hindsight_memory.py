2ee2d82445ea08eab8c2fcaf29451f2940e1a416
Udit007-G <thebillonaire02@gmail.com>
2026-06-14 18:05:13 +0530

    Version 2

---
+++ hindsight_memory.py
@@ -0,0 +1,22 @@
+from hindsight_client import Hindsight
+from config import HINDSIGHT_API_KEY
+
+client = Hindsight(
+    base_url="https://api.hindsight.vectorize.io",
+    api_key=HINDSIGHT_API_KEY
+)
+
+client.retain(
+    bank_id="code-review-memory",
+    content="""
+Issue: Hardcoded Password
+
+Suggestion:
+Use environment variables.
+
+Status:
+Ignored
+"""
+)
+
+print("Memory Stored")
